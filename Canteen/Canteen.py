95e354ab0e740a946d8824c90aee3909cc949447
shaik sameer <19kb1a1244@nbkrist.org>
2022-07-13 20:50:56 +0530

    adding imgs into database

--- Canteen/Canteen.py
+++ Canteen/Canteen.py
@@ -1,5 +1,5 @@
 import datetime
-
+import mysql.connector as sql
 from flask import *
 
 canteen=Blueprint('canteen',__name__)
@@ -12,13 +12,36 @@ def menu():
     return render_template('canteen/menu.html')
 @canteen.route('/breakfast')
 def breakfast():
-    return render_template('canteen/breakfast.html')
+    con = sql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="password",
+        database="MINI_PROJECT"
+
+    )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM ITEMS WHERE ITEM_CATEGORY='BREAKFAST'")
+    breakfast_items=cur.fetchall()
+    con.close()
+
+    return render_template('canteen/breakfast.html',items=breakfast_items)
 @canteen.route('/dinner')
 def dinner():
     return render_template('canteen/dinner.html')
 @canteen.route('/lunch')
 def lunch():
-    return render_template('canteen/lunch.html')
+    con = sql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="password",
+        database="MINI_PROJECT"
+
+    )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM ITEMS WHERE ITEM_CATEGORY='LUNCH'")
+    lunch_items=cur.fetchall()
+    con.close()
+    return render_template('canteen/lunch.html',items=lunch_items)
 @canteen.route('/special')
 def special():
     return render_template('canteen/todayspl.html')
