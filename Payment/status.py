e48bdc2a013e209fd45def1c2b7b9ee9a44463a8
shaik sameer <19kb1a1244@nbkrist.org>
2022-07-18 22:50:09 +0530

    demo version

--- Payment/status.py
+++ Payment/status.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session,request
 import mysql.connector as sql
+import pdfkit
+
 
 status = Blueprint('status', __name__)
 
@@ -16,9 +18,26 @@ def all_status():
         )
     cur = con.cursor()
     cur.execute(
-        "SELECT STATUS.ROWID, STUDENT_INFO.NAME,STUDENT_INFO.ROLLNO,STATUS.AMOUNT,STATUS.CATEGORY,STATUS.DATE,STATUS.STATUS FROM STATUS INNER JOIN STUDENT_INFO ON STATUS.ROLLNO=STUDENT_INFO.ROLLNO WHERE STATUS.ROLLNO= '%s' " % (
+        "SELECT STATUS.ROWID, STUDENT_INFO.NAME,STUDENT_INFO.ROLLNO,STATUS.AMOUNT,STATUS.CATEGORY,STATUS.DATE,STATUS.STATUS,STATUS.TOKEN_NO,STATUS.Order_Status FROM STATUS INNER JOIN STUDENT_INFO ON STATUS.ROLLNO=STUDENT_INFO.ROLLNO WHERE STATUS.ROLLNO= '%s' " % (
             roll_no))
     data = cur.fetchall()
-    print(data)
     con.close()
     return render_template('status.html', status1=data)
+@status.route('/reciept/<reciept_id>')
+def reciept(reciept_id):
+    con = sql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="password",
+        database="MINI_PROJECT"
+
+    )
+    cur = con.cursor()
+    cur.execute(
+        "SELECT STATUS.ROWID, STUDENT_INFO.NAME,STUDENT_INFO.ROLLNO,STUDENT_INFO.CLASS,STUDENT_INFO.ACADEMIC,STATUS.AMOUNT,STATUS.CATEGORY,STATUS.DATE FROM STATUS INNER JOIN STUDENT_INFO ON STATUS.ROLLNO=STUDENT_INFO.ROLLNO WHERE STATUS.ROWID= '%s' " % (reciept_id))
+    data=cur.fetchone()
+    return render_template('feereceipt.html',data=data)
+@status.route('/download')
+def download():
+    link=request.referrer
+    pdfkit.from_url(link, 'connect.pdf')
--- demo.py
+++ demo.py
@@ -1,1 +1,10 @@
-
+import mysql.connector as sql
+con = sql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="password",
+        database="MINI_PROJECT"
+    )
+cur = con.cursor()
+cur.execute('SELECT * FROM ITEM_ORDERS WHERE STATUS=0')
+print(cur.fetchall())
